f37d47a2911e55b9c0121c1b629caf0e468fbbbc
fxhdhdydcghgg <fxhdhdydcghgg@gmail.com>
2026-09-15 09:36:00 +0300

    это надо

--- LR1MDK0101/LR1/LR1/Task1.cs
+++ LR1MDK0101/LR1/LR1/Task1.cs
@@ -10,9 +10,47 @@ namespace LR1
     {
         public static void TaskStart()
         {
-            int orderCost = InputNumber.Input("стоимость заказа (руб.)");
-            int distance = InputNumber.Input("расстояние доставки (км)");
-            int time = InputNumber.Input("время заказа (час)");
+            int orderCost = -1;
+            while (orderCost < 0) {
+                InputNumber.Input("расстояние доставки (км)");
+            }
+            try
+            {
+                InputNumber.Input("расстояние доставки (км)");
+            }
+            catch (FormatException)
+            {
+
+                InputNumber.Input("расстояние доставки (км)");
+            }
+            int distance = -1;
+            while (distance < 0)
+            {
+                InputNumber.Input("расстояние доставки (км)");
+            }
+            try
+            {
+                InputNumber.Input("расстояние доставки (км)");
+            }
+            catch (FormatException)
+            {
+
+                InputNumber.Input("расстояние доставки (км)");
+            }
+            int time = -1;
+            while (time >23 || time <0)
+            {
+                time = InputNumber.Input("время заказа (час)");
+            }
+            try
+            {
+                InputNumber.Input("расстояние доставки (км)");
+            }
+            catch (FormatException)
+            {
+
+                time = InputNumber.Input("время заказа (час)");
+            }
             float deliveryCost = CalculateCost.CalculateDelivery(orderCost, distance, time);
             Console.WriteLine("Стоимость доставки: " + deliveryCost);
             Console.WriteLine("Итого к оплате: " + CalculateCost.CalculateTotal(deliveryCost, orderCost));
