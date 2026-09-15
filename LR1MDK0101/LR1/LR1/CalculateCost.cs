2157a230aacd749efa9f2afcd2ea92419dcc5614
fxhdhdydcghgg <fxhdhdydcghgg@gmail.com>
2026-09-15 08:58:32 +0300

    Создал проект, создал документ, Добавил модуль ввода чисел, Добавил модуль подсчёта цен с функцией подсчёта цены доставки.

---
+++ LR1MDK0101/LR1/LR1/CalculateCost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    internal class CalculateCost
+    {
+
+        public static float CalculateDelivery(int orderSum, int distanceKm, int time)
+        {
+            if (orderSum >= 2000) return 0;
+            int baseCost = 150;
+            float finalCost = 0;
+            finalCost = distanceKm > 3 ? baseCost * (distanceKm - 3) : baseCost;
+            finalCost = (time >= 12 && time <= 14 || time >= 18 && time <= 20) ? finalCost * 1.3f : finalCost;
+            return finalCost;
+        }
+       
+    }
+}
---
+++ LR1MDK0101/LR1/LR1/InputNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    internal class InputNumber
+    {
+        public static int Input(string neededData)
+        {
+            Console.WriteLine($"Введите {neededData}: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
