16582135afd4b369fea17e35419bd6733ef0349e
fxhdhdydcghgg <fxhdhdydcghgg@gmail.com>
2026-09-15 09:37:21 +0300

    оно было не надо

--- LR1MDK0101/LR1/LR1/Task1.cs
+++ LR1MDK0101/LR1/LR1/Task1.cs
@@ -14,41 +14,14 @@ namespace LR1
             while (orderCost < 0) {
                 InputNumber.Input("расстояние доставки (км)");
             }
-            try
-            {
-                InputNumber.Input("расстояние доставки (км)");
-            }
-            catch (FormatException)
-            {
-
-                InputNumber.Input("расстояние доставки (км)");
-            }
             int distance = -1;
             while (distance < 0)
             {
                 InputNumber.Input("расстояние доставки (км)");
             }
-            try
-            {
-                InputNumber.Input("расстояние доставки (км)");
-            }
-            catch (FormatException)
-            {
-
-                InputNumber.Input("расстояние доставки (км)");
-            }
             int time = -1;
             while (time >23 || time <0)
             {
-                time = InputNumber.Input("время заказа (час)");
-            }
-            try
-            {
-                InputNumber.Input("расстояние доставки (км)");
-            }
-            catch (FormatException)
-            {
-
                 time = InputNumber.Input("время заказа (час)");
             }
             float deliveryCost = CalculateCost.CalculateDelivery(orderCost, distance, time);
