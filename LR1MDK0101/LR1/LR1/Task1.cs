fe50c45842436d9faf1564ec1e8aa6c71bc94338
fxhdhdydcghgg <fxhdhdydcghgg@gmail.com>
2026-09-15 09:08:21 +0300

    Добавил модуль вызова модулей для первой задачи

---
+++ LR1MDK0101/LR1/LR1/Task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    internal class Task1
+    {
+        public static void TaskStart()
+        {
+            int orderCost = InputNumber.Input("стоимость заказа");
+            int distance = InputNumber.Input("расстояние доставки");
+            int time = InputNumber.Input("время заказа");
+            float deliveryCost = CalculateCost.CalculateDelivery(orderCost, distance, time);
+            Console.WriteLine("Стоимость доставки: " + deliveryCost);
+            Console.WriteLine("Итого к оплате: " + CalculateCost.CalculateTotal(deliveryCost, orderCost));
+        }
+    }
+}
